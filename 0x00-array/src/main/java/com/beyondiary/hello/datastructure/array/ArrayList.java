59bd05fbcaf2f896afa4f25201695703937355f3
Mark Chung <beyondefined@gmail.com>
2021-01-30 23:53:09 +0800

    fix ArrayList remove void shock

---
+++ 0x00-array/src/main/java/com/beyondiary/hello/datastructure/array/ArrayList.java
@@ -0,0 +1,158 @@
+package com.beyondiary.hello.datastructure.array;
+
+import java.util.Objects;
+
+/**
+ * @author Beyondiary
+ * @author <a href="mailto:beyondefined@gmail.com">Mark Chung</a>
+ * @date 2021-01-30 01:41
+ */
+public class ArrayList<E> {
+    private E[] data;
+    private int size;
+
+    public ArrayList(int capacity) {
+        this.data = (E[]) new Object[capacity];
+        this.size = 0;
+    }
+
+    public ArrayList() {
+        this(10);
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getCapacity(){
+        return data.length;
+    }
+
+    public boolean isEmpty(){
+        return 0 == size;
+    }
+
+    public boolean contains(E e){
+        for (int i = 0; i < size; i++) {
+            if(data[i].equals(e)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int find(E e){
+        for (int i = 0; i < size; i++) {
+            if(data[i].equals(e)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public E remove(int index){
+        if(index < 0 || index >=size){
+            throw new IllegalArgumentException("index illegal");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i -1] = data[i];
+        }
+        size--;
+        data[size] = null;
+
+        if(size == data.length/4 && data.length/2 != 0){
+            resize(data.length/2);
+        }
+        return ret;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e){
+        int index = find(e);
+        if(index != -1){
+            remove(index);
+        }
+    }
+
+    public void removeAllElement(E e){
+        int index = find(e);
+        while (index != - 1){
+            remove(index);
+            index = find(e);
+        }
+    }
+
+    public void addLast(E e){
+        /*
+        if(size == data.length){
+            throw new IllegalArgumentException("array is full");
+        }
+        data[size] = e;
+        size++;
+        */
+        add(size, e);
+    }
+
+    public void addFirst(E e){
+        add(0, e);
+    }
+
+    public E get(int index){
+        if(index < 0 || index >=size){
+            throw new IllegalArgumentException("index illegal");
+        }
+        return data[index];
+    }
+
+    public void set(int index, E e){
+        if(index < 0 || index >=size){
+            throw new IllegalArgumentException("index illegal");
+        }
+        data[index] = e;
+    }
+
+    public void add(int index, E e){
+        if(index < 0 || index > size){
+            throw new IllegalArgumentException("index illegal");
+        }
+        if(size == data.length){
+            resize(2 * data.length);
+        }
+        for (int i = size - 1; i >=index ; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+    }
+
+    private void resize(final int newCapacity) {
+        E[] newData = (E[]) new Objects[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("array size %d, capacity %d \n", size, data.length));
+        res.append('[');
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if(i != size - 1){
+                res.append(", ");
+            }
+        }
+        res.append(']');
+        return res.toString();
+    }
+}
